22a55f4549f2e959f24892da0f87f6eb6107cb57
ekefan <155914219+ekefan@users.noreply.github.com>
2024-02-13 00:30:54 +0000

    checks

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-
 JOBS = [{
     'id': 1,
     'title': 'Social media strategist',
@@ -18,17 +17,33 @@ JOBS = [{
     'location': 'Remote',
     'salary': '25k',
 }]
-
-
 @app.route('/')
-def hello_world():
+def home_page():
   return render_template('home.html', jobs=JOBS)
+
+
+@app.route('/job/<id>')
+def show_jobs(id):
+  # job = load_job_from_db(id)
+  job = JOBS[id]
+
+  if not job:
+    return 'Not found', 404
+  return render_template('jobpage.html', job=job)
+
 
 #instead of returning data to render html
 #this is used to create api end points
 @app.route('/api/jobs')
 def list_jobs():
   return jsonify(JOBS)
+
+# @app.route('/job/<id>/apply', method="post")
+# def apply_to_job(id):
+#   # data = request.args what is the difference between request.form and request.args
+#   data = request.form
+#   return jsonify(data)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
